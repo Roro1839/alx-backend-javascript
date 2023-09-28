c331d2588e86dc93d3b8b2b6aab4655670b11481
Roro1839 <ronaldokipkirui90@gmail.com>
2023-09-27 17:22:45 -0700

    es6

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -4,9 +4,9 @@ export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     // eslint-disable-next-line no-constant-condition
     if (true) {
-      resolve({ status: 200, body: "Success"});
+      resolve({ status: 200, body: 'Success'});
     } else {
-      reject(new Error("The fake API is not working currently"));
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
