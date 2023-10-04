c67295dc9539bca2bd9a015b56a67f9b3c24caa0
Roro1839 <ronaldokipkirui90@gmail.com>
2023-10-04 16:22:21 -0700

    es6

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,22 @@
+interface Student {
+	firstName: string;
+	lastName: string;
+        age: number;
+        location: string;
+}
+
+const stud1: Student {
+	firstName: "Robin";
+	lastName: "Langat";
+	age: 19;
+	location: "Litein";
+}
+
+const stud2: Student {
+        firstName: "Alex";
+        lastName: "Cheruiyot";
+        age: 19;
+        location: "Litein";
+}
+
+const studlist: Student[] = [ stud1, stud2];
